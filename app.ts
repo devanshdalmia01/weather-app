135843045784e98d783b51e2ea40fe28f6aaea6b
devanshdalmia01 <devanshdalmia1@gmail.com>
2023-01-31 16:43:03 +0530

    testing something

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 import weatherRoutes from "./router/weatherRoutes.js";
 
 const app = express();
-
+app.set("trust proxy", true);
 app.use("/api", weatherRoutes);
 
 export default app;
