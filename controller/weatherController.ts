b63e63632d5c0b04f8269746af88faf75108bd3e
devanshdalmia01 <devanshdalmia1@gmail.com>
2023-02-03 14:30:07 +0530

    mm

--- controller/weatherController.ts
+++ controller/weatherController.ts
@@ -4,6 +4,34 @@ import { getWeatherFromCityCode } from "../helpers/getWeatherAPI.js";
 import geoip from "geoip-lite";
 
 const getWeatherDataForCity = async (req: Request, res: Response) => {
+	console.log(req.ip);
+	// GeoIP Package
+	console.time();
+	console.log(geoip.lookup(req.ip));
+	console.timeEnd();
+	// Key CDN
+	console.time();
+	console.log(
+		await (
+			await fetch(`https://tools.keycdn.com/geo.json?host=${req.ip}`, {
+				method: "GET",
+				headers: {
+					"User-Agent": "keycdn-tools:https://.*",
+				},
+			})
+		).json()
+	);
+	console.timeEnd();
+	// IP Info
+	console.time();
+	console.log(
+		await (
+			await fetch(`https://ipinfo.io/${req.ip}/?token=5de107c6c57581`, {
+				method: "GET",
+			})
+		).json()
+	);
+	console.timeEnd();
 	if (req.query.cityCode === undefined) {
 		getWeatherFromCityCode(1259229, res);
 	} else {
